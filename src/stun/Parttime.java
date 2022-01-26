74155527d4334b86e8b29aced537385d7d331b58
college <college@DESKTOP-RG1RU0U.phub.net.cable.rogers.com>
2022-01-26 14:19:31 -0500

    parttimestudent

---
+++ src/stun/Parttime.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stun;
+
+/**
+ *
+ * @author college
+ */
+public class Parttime {
+
+    /**
+     * @return the numoffCourses
+     */
+    public int getNumoffCourses() {
+        return numoffCourses;
+    }
+
+    /**
+     * @param numoffCourses the numoffCourses to set
+     */
+    public void setNumoffCourses(int numoffCourses) {
+        this.numoffCourses = numoffCourses;
+    }
+    private int numoffCourses;
+    
+}
